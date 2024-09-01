f530ca63c35c5f1922eb450fb2a53b51d0975490
NgoTheVinh <vinhnthe170433@fpt.edu.vn>
2024-09-02 00:22:52 +0700

    Complete shop system/ deduct resource when purchase

---
+++ My project/Assets/Scripts/ShopButton.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopButton : MonoBehaviour
+{
+
+    public int bloodCost;
+    public int woodCost;
+    public int crystalCost;
+    Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ResourceManager.instance.blood < bloodCost || 
+            ResourceManager.instance.wood < woodCost||
+            ResourceManager.instance.crystal < crystalCost)
+        {
+            button.interactable = false;
+        }
+        else
+        {
+            button.interactable= true;
+        }
+        
+    }
+
+    public void RemoveResource()
+    {
+        ResourceManager.instance.AddResource("blood", -bloodCost);
+        ResourceManager.instance.AddResource("wood", -woodCost);
+        ResourceManager.instance.AddResource("crystal", -crystalCost);
+    }
+}
